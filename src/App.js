577a7e108f89b6129349b3e2010d56bc04242aed
James Thigpen <james.r.thigpen@gmail.com>
2024-10-08 16:15:41 -0600

    Beantime baby

--- src/App.js
+++ src/App.js
@@ -10,19 +10,30 @@ import moment from "moment";
 function Time() {
 
   const [time, setTime] = React.useState(moment.utc());
- 
-   React.useEffect(() => {
-     setInterval(() => {
+
+  React.useEffect(() => {
+    setInterval(() => {
       setTime(moment.utc());
-     }, 1000);
-   }, []);
- 
-  return <span>{time.utcOffset("-04:58:17").format("dddd, MMMM Do YYYY, HH:mm:ss")} </span>;
-  }
+    }, 1000);
+  }, []);
+
+  return <span>{time.utcOffset("-04:58:17").format("dddd, MMMM Do YYYY, HH:mm:ss")}</span>;
+}
 
 export default function App() {
   return (
-    <><div id="clock"><Time></Time></div></>
-    
+    <>
+      <div>
+        <div id="clock"><Time></Time> BT</div>
+      </div>
+
+      <footer class="fixed bottom-0 left-0 z-20 w-full p-4">
+        <div class="text-sm text-gray-500 text-center sm:text-center dark:text-gray-400">
+          <span>Bean Time is the Official Time Zone of the Furry Universe</span>
+        </div>
+      </footer>
+
+    </>
+
   )
 }
